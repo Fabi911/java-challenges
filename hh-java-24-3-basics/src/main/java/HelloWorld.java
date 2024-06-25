cd0c1bab40123f4455ba860a88527c1943898e28
Fabian Döz <fabian@doez.info>
2024-06-25 17:02:16 +0200

    inital upload

---
+++ hh-java-24-3-basics/src/main/java/HelloWorld.java
@@ -0,0 +1,28 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Java!");
+
+        int a = 30;
+        int b = 20;
+        int sum = a * b;
+
+        System.out.println("sum " + sum);
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a / b);
+        System.out.println(a * b);
+        System.out.println(a < b);
+        System.out.println(a > b);
+        System.out.println(a == b);
+
+        float c = 3.14f;
+        double d = 3.14;
+        System.out.println(c + d);
+        System.out.println(c - d);
+        System.out.println(c / d);
+        System.out.println(c * d);
+        System.out.println(c / d);
+        System.out.println(d > c);
+
+    }
+}
---
+++ hh-java-24-3-basics/src/main/java/MyFirstJavaApplication.java
@@ -0,0 +1,16 @@
+public class MyFirstJavaApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello hh-java-24-3");
+
+        String name = "Fabian";
+
+        System.out.println(name);
+
+        int age = 34;
+        double myDouble = 3.14;
+        boolean myBoolean = true;
+
+        System.out.println(age + " und " + myDouble + " ist " + myBoolean);
+    }
+}
