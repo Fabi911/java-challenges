1ede067e28d4949431634bf3beecb2f87d4e209d
Fabian Döz <fabian@doez.info>
2024-06-26 12:33:23 +0200

    cleanCode

--- 1-Java/03-If-Else-Method/src/main/java/addMethod.java
+++ 1-Java/03-If-Else-Method/src/main/java/addMethod.java
@@ -6,8 +6,8 @@ public class addMethod {
         System.out.println("subtract: "+ subtract(3,4));
         System.out.println("subtract: "+ subtract(3.8,2.6));
         System.out.println("makePositive: "+ makePositive(3,4));
-        System.out.println("isABigger: "+ isABigger(3,4));
-        System.out.println("isABigger: "+ isABigger(4,2));
+        isABigger(3,4);
+        isABigger(4,2);
         System.out.println("oddNumberCheck: "+oddNumberCheck(4));
         System.out.println("oddNumberCheck: "+oddNumberCheck(7));
         System.out.println(isPalindrome("anna"));
@@ -31,11 +31,11 @@ public class addMethod {
     public static int makePositive(int a, int b) {
         return Math.abs(subtract(a, b));}
 
-    public static int isABigger(int a, int b) {
+    public static void isABigger(int a, int b) {
         if (a > b) {
-            return subtract(a, b);
-        } else {
-            return add(a, b);
+            System.out.println("isABigger: "+subtract(a, b));
+        } else if (a < b) {
+            System.out.println("A is smaller: "+ add(a,b));
         }
     }
 
