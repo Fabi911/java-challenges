3b871e2520a1f31edc50748aacb83e9466cfd830
Fabian Döz <fabian@doez.info>
2024-06-26 11:35:14 +0200

    03-If-Else-Methode

---
+++ 1-Java/03-If-Else-Method/src/main/java/addMethod.java
@@ -0,0 +1,62 @@
+public class addMethod {
+
+    public static void main(String[] args) {
+        System.out.println("add: "+ add(3,4));
+        System.out.println("subtract: "+ subtract(3,4));
+        System.out.println("makePositive: "+ makePositive(3,4));
+        System.out.println("isABigger: "+ isABigger(3,4));
+        System.out.println("isABigger: "+ isABigger(4,2));
+        System.out.println("oddNumberCheck: "+oddNumberCheck(4));
+        System.out.println("oddNumberCheck: "+oddNumberCheck(7));
+        System.out.println(isPalindrome("anna"));
+        System.out.println(isPalindrome("car"));
+        System.out.println(isPalindrome("racecar"));
+
+    }
+    public static int add(int a, int b) {
+        return a + b;
+    }
+    public static double add(double a, double b) {
+        return a + b;
+    }
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+    public static double subtract(double a, double b) {
+        return a - b;
+    }
+
+    public static int makePositive(int a, int b) {
+        return Math.abs(subtract(a, b));}
+
+    public static int isABigger(int a, int b) {
+        if (a > b) {
+            return subtract(a, b);
+        } else {
+            return add(a, b);
+        }
+    };
+
+    public static int oddNumberCheck(int number) {
+        if (number % 2 == 0) {
+            return number;
+        } else {
+            return number * 2;
+        }
+    };
+
+    public static boolean isPalindrome(String text) {
+        int left = 0;
+        int right = text.length() - 1;
+
+        while (left < right) {
+
+            if (text.charAt(left) != text.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
